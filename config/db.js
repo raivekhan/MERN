0645c89fcecf4a288972695c17639f0ae1f72a78
Naimur <raivekhan@gmail.com>
2021-07-15 15:13:56 +0600

    Connect to MongoDB with mongoose

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoUri");
+
+const connectDB = async () => {
+  try {
+    console.log("Db ", db);
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("MongoDB connected....");
+  } catch (error) {
+    console.error("DB connection failed");
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
