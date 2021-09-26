80e1f4fdc2910937734d6df29d41f7e900f1fb70
Naimur <raivekhan@gmail.com>
2021-09-26 19:14:52 +0600

    Created profile model and get current user profile from auth-token

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,31 @@
 const express=require('express');
 const router=express.Router();
 
+const Profile = require('../../models/Profile');
+const authMiddleware= require('../../middleware/auth')
 
 // @route GET /profile
 // @desc test route
 // @access public
-router.get('/', (req, res)=>{
+router.get('/', authMiddleware, (req, res)=>{
 res.send('Profile route');
 })
+
+// @route GET /profile/me
+// @desc Get current user profile
+// @access private
+router.get('/me', authMiddleware, async (req, res)=>{
+    try {
+        const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar']);
+
+        if(!profile) {
+            res.status(400).json({msg: 'There is no profile for this user'});
+        }
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error')
+    }
+    })
 
 module.exports=router;
